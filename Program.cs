edd53a0eecb48bba73729b22005aea0fc79dbe9d
«DimaG» <galkin2002@yandex.ru>
2024-03-20 14:50:48 +0300

    Добавлено решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите элементы массива (через пробел):");
+string input = Console.ReadLine();
+string[] inputArray = input.Split(' ');
+
+int count = 0;
+for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+string[] resultArray = new string[count];
+int index = 0;
+for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            resultArray[index] = inputArray[i];
+            index++;
+        }
+    }
+
+Console.WriteLine();
+
+Console.Write("Массив: [");
+for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (i != 0)
+            Console.Write(", ");
+        Console.Write("\"" + inputArray[i] + "\"");
+    }
+Console.WriteLine("]");
+
+Console.WriteLine();
+
+Console.Write("Результат работы программы: [");
+for (int i = 0; i < resultArray.Length; i++)
+    {
+        if (i != 0)
+            Console.Write(", ");
+        Console.Write("\"" + resultArray[i] + "\"");
+    }
+    Console.WriteLine("]");
